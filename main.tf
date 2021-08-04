87b11a6b3f8058648bac898b0bd1bec033701ebc
Sean Sundberg <seansund@us.ibm.com>
2021-08-03 22:50:16 -0500

    Adds logic to deploy tekton resources via gitops (#1)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
 locals {
-  name      = "my-module"
-  chart_dir = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
-  ingress_host  = "${local.name}-${var.namespace}.${var.cluster_ingress_hostname}"
-  ingress_url   = "https://${local.ingress_host}"
-  service_url  = "http://${local.name}.${var.namespace}"
-  values_content = {
-  }
+  name      = "tekton-resources"
+  resource_dir = "${path.cwd}/.tmp/${local.name}"
   layer = "services"
   application_branch = "main"
   layer_config = var.gitops_config[local.layer]
@@ -13,11 +8,7 @@ locals {
 
 resource null_resource setup_chart {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/create-yaml.sh '${local.name}' '${local.chart_dir}'"
-
-    environment = {
-      VALUES_CONTENT = yamlencode(local.values_content)
-    }
+    command = "${path.module}/scripts/create-yaml.sh '${local.name}' '${local.resource_dir}' '${var.task_release}'"
   }
 }
 
@@ -25,7 +16,7 @@ resource null_resource setup_gitops {
   depends_on = [null_resource.setup_chart]
 
   provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-gitops.sh '${local.name}' '${local.chart_dir}' '${local.name}' '${local.application_branch}' '${var.namespace}'"
+    command = "${path.module}/scripts/setup-gitops.sh '${local.name}' '${local.resource_dir}' '${local.name}' '${local.application_branch}' '${var.namespace}'"
 
     environment = {
       GIT_CREDENTIALS = jsonencode(var.git_credentials)

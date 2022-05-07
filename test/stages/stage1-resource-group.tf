bad8e0bd7e71f4a8a4a115f3d61033be1db2e417
balasgit <bala@us.ibm.com>
2022-05-07 14:16:21 -0500

    Fix the build error

--- test/stages/stage1-resource-group.tf
+++ test/stages/stage1-resource-group.tf
@@ -2,5 +2,9 @@ module "resource_group" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-resource-group.git"
 
   resource_group_name = var.resource_group_name
-  provision           = false
+  
+  #provision           = false
+
+  ## Added to fix build error
+  ibmcloud_api_key = ""
 }

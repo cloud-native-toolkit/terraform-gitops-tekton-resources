cdf5fb53c85853dbb33f742de16d0a4f4f442593
balasgit <bala@us.ibm.com>
2022-05-11 10:14:44 -0500

    Added provider block & Hashcorp version

--- version.tf
+++ version.tf
@@ -1,23 +1,4 @@
 terraform {
   required_version = ">= 0.15.0"
 
-  required_providers {
-    gitops = {
-      source = "cloud-native-toolkit/gitops"
-    }
-    hashicorp-local = {
-      source  = "hashicorp/local"
-      version = "= 2.2.2"
-    }
-    hashicorp-external = {
-      source = "hashicorp/external"
-      version = "2.2.2"
-    }
-    
-    hashicorp-ibm = { 
-      source = "IBM-Cloud/ibm"
-      version = "1.41.0"
-    }
- }
-
 }

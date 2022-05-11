9b3952a8c7be6bc6ca8989ce66fe88df347b0189
balasgit <bala@us.ibm.com>
2022-05-11 09:20:15 -0500

    added version

--- version.tf
+++ version.tf
@@ -1,3 +1,18 @@
 terraform {
   required_version = ">= 0.15.0"
+
+  required_providers {
+    gitops = {
+      source = "cloud-native-toolkit/gitops"
+    }
+    hashicorp-local = {
+      source  = "hashicorp/local"
+      version = "= 2.2.2"
+    }
+    hashicorp-external = {
+      source = "hashicorp/external"
+      version = "2.2.2"
+    }
+  }
+
 }
